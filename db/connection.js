35626dba96021b1bc3f23d29121e533c986c82f0
philaung96 <philaung96@gmail.com>
2021-11-23 11:21:58 -0800

    heroku connection

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/list', { useNewUrlParser: true }, () => {
-	console.log('we in');
-});
+// mongoose.connect('mongodb://localhost/list', { useNewUrlParser: true }, () => {
+// 	console.log('we in');
+// });
+
+mongoose.Promise = Promise;
+let mongoURI = '';
+if (process.env.NODE_ENV === 'production') {
+	mongoURI = process.env.DB_URI;
+} else {
+	mongoURI = 'mongodb://localhost/todolist';
+}
+
+mongoose
+	.connect(mongoURI, { useNewUrlParser: true })
+	.then((instance) => {
+		console.log(`Connected to db: ${instance.connections[0].name}`);
+	})
+	.cath((error) => {
+		console.log('Connection failed', error);
+	});
 
 module.exports = mongoose;
+
+//'mongodb+srv://admin:adminPassword@cluster0.74tjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
